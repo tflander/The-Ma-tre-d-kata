af7279a7187e2098ab377c6eeb0f1dc32db8b5ba
Todd Flanders <todd.a.flanders@accenture.com>
2020-01-28 21:55:45 -0500

    add tests for seating parties together

--- tests/test_table.py
+++ tests/test_table.py
@@ -19,12 +19,16 @@ def test_takes_reservation_for_empty_table(twelve_top):
 
 def test_does_not_take_reservation_for_full_table(twelve_top):
     twelve_top.request_reservation_for(12)
-    assert twelve_top.seats == 12
     assert twelve_top.available == 0
     assert twelve_top.request_reservation_for(1) == False
 
 
 def test_does_not_take_reservation_for_more_than_the_table_holds(twelve_top):
     assert twelve_top.request_reservation_for(13) == False
-    assert twelve_top.seats == 12
     assert twelve_top.available == 12
+
+
+def test_seats_different_parties_together(twelve_top):
+    assert twelve_top.request_reservation_for(3)
+    assert twelve_top.request_reservation_for(4)
+    assert twelve_top.available == 5
