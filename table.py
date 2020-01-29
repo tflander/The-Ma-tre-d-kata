a376bc65f2d3d66ef8d6eb1e7af4588390c10e0a
Todd Flanders <todd.a.flanders@accenture.com>
2020-01-28 22:03:31 -0500

    add reservation date

--- table.py
+++ table.py
@@ -1,9 +1,10 @@
+
 class Table:
     def __init__(self, seats):
         self.seats = seats
         self.available = seats
 
-    def request_reservation_for(self, requested_count):
+    def request_reservation_for(self, reservation_date, requested_count):
         if requested_count <= self.available:
             self.available -= requested_count
             return True
--- tests/test_table.py
+++ tests/test_table.py
@@ -1,9 +1,14 @@
 import table
 import pytest
+import datetime
+
 
 @pytest.fixture
 def twelve_top():
     return table.Table(seats=12)
+
+
+new_years_day = datetime.date(2030, 1, 1)
 
 
 def test_init(twelve_top):
@@ -12,23 +17,23 @@ def test_init(twelve_top):
 
 
 def test_takes_reservation_for_empty_table(twelve_top):
-    assert twelve_top.request_reservation_for(10)
+    assert twelve_top.request_reservation_for(new_years_day, requested_count=10)
     assert twelve_top.seats == 12
     assert twelve_top.available == 2
 
 
 def test_does_not_take_reservation_for_full_table(twelve_top):
-    twelve_top.request_reservation_for(12)
+    twelve_top.request_reservation_for(new_years_day, requested_count=12)
     assert twelve_top.available == 0
-    assert twelve_top.request_reservation_for(1) == False
+    assert twelve_top.request_reservation_for(new_years_day, requested_count=1) == False
 
 
 def test_does_not_take_reservation_for_more_than_the_table_holds(twelve_top):
-    assert twelve_top.request_reservation_for(13) == False
+    assert twelve_top.request_reservation_for(new_years_day, requested_count=13) == False
     assert twelve_top.available == 12
 
 
 def test_seats_different_parties_together(twelve_top):
-    assert twelve_top.request_reservation_for(3)
-    assert twelve_top.request_reservation_for(4)
+    assert twelve_top.request_reservation_for(new_years_day, requested_count=3)
+    assert twelve_top.request_reservation_for(new_years_day, requested_count=4)
     assert twelve_top.available == 5
