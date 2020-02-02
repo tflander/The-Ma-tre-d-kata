bbec3e76c764261ed2e2c9cafc54d73ac02a84a8
Todd Flanders <todd.a.flanders@accenture.com>
2020-02-02 17:00:47 -0500

    create reservation class

---
+++ reservation.py
@@ -0,0 +1,6 @@
+class Reservation:
+
+    def __init__(self, reservation_date, table, number_of_reserved_seats):
+        self.reservation_date = reservation_date
+        self.table = table
+        self.number_of_reserved_seats = number_of_reserved_seats
--- restaurant.py
+++ restaurant.py
@@ -1,12 +1,12 @@
 from enum import Enum
+import reservation
 
 class RestaurantType(Enum):
     BOUTIQUE = "boutique (shared seating)"
-    HAUsTE_CUISINE = "single seating"
-    SECOND_SEATING = "2 1/2 hours per seating"
+    # HAUSTE_CUISINE = "single seating"
+    # SECOND_SEATING = "2 1/2 hours per seating"
 
 class Restaurant:
-
 
     def __init__(self, restaurant_type, table):
         self.reservations = dict()
@@ -16,16 +16,16 @@ class Restaurant:
 
         current_seats_reserved_count = 0;
         if reservation_date in self.reservations:
-            current_seats_reserved_count = self.reservations[reservation_date]
+            current_seats_reserved_count = self.reservations[reservation_date].number_of_reserved_seats
 
         if requested_count + current_seats_reserved_count <= self.table.seats:
-            self.reservations[reservation_date] = requested_count + current_seats_reserved_count
+            self.reservations[reservation_date] = reservation.Reservation(reservation_date, self.table, requested_count + current_seats_reserved_count)
             return True
         else:
             return False
 
     def available_for(self, reservation_date):
         if reservation_date in self.reservations:
-            return self.table.seats - self.reservations[reservation_date]
+            return self.table.seats - self.reservations[reservation_date].number_of_reserved_seats
         else:
             return self.table.seats
